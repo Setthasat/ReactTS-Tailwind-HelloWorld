dfc97f697617f01664cd7623422269d7adf7cbee
setthasat <setthasat2007@gmail.com>
2023-11-16 18:59:41 +0700

    add search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+import './SearchBar.css';
 
 const SearchBar: React.FC = () => {
+
+
 
     const [search, setSearch] = useState({
         title: ''
@@ -22,20 +27,28 @@ const SearchBar: React.FC = () => {
     };
 
     return (
-        <div className="absolute top-[45%] mx-[30%]">
-            <div className="bg-white w-[45rem] h-[5rem] rounded-full">
+        <div className='absolute top-[45%] mx-[30%]'>
+
+            <div className="w-[45rem] h-[5rem] rounded-full">
                 <form onSubmit={handleSubmit}>
-                    <label>
-                        <input
+                    <motion.label
+
+                    >
+                        <motion.input
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 3 }}
                             type="text"
                             name='title'
                             value={search.title}
                             onChange={onChangeInput}
                             className="text-[2rem] rounded-full font-bold h-[5rem] min-w-full shadow-2xl  pl-[4rem] placeholder:-px-[20px] placeholder:font-bold focus:outline-none focus:ring-slate-800 focus:ring-2 font-sans" />
-                    </label>
+                    </motion.label>
                 </form>
             </div>
+
         </div>
+
     );
 };
 
