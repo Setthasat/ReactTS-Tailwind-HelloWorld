4b374324ce05d5971d0e70c34932412f96e58508
setthasat <setthasat2007@gmail.com>
2023-11-20 20:48:29 +0700

    add client folder

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -28,21 +28,19 @@ const SearchBar: React.FC = () => {
 
     return (
         <div className='absolute top-[45%] mx-[30%]'>
-
             <div className="w-[45rem] h-[5rem] rounded-full">
                 <form onSubmit={handleSubmit}>
-                    <motion.label
-
-                    >
+                    <motion.label>
                         <motion.input
                             initial={{ opacity: 0, scale: 0.5 }}
                             animate={{ opacity: 1, scale: 1 }}
-                            transition={{ duration: 3 }}
+                            transition={{ duration: 2 }}
                             type="text"
                             name='title'
                             value={search.title}
                             onChange={onChangeInput}
-                            className="text-[2rem] rounded-full font-bold h-[5rem] min-w-full shadow-2xl  pl-[4rem] placeholder:-px-[20px] placeholder:font-bold focus:outline-none focus:ring-slate-800 focus:ring-2 font-sans" />
+                            placeholder="Why u read bái chī ?"
+                            className="text-[2rem] rounded-full font-bold h-[5rem] min-w-full shadow-2xl  pl-[14rem] placeholder:-px-[20px] placeholder:font-bold focus:outline-none focus:ring-slate-800 focus:ring-2 font-sans" />
                     </motion.label>
                 </form>
             </div>
